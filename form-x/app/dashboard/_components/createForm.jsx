3fd0c9d51b758a48de19f9c122eb5cb9ba7707d3
marsyg <net.maaz@yahoo.com>
2025-01-09 01:15:39 +0530

    add dotenv and moment dependencies, implement database connection and form generation logic in CreateForm component

--- form-x/app/dashboard/_components/createForm.jsx
+++ form-x/app/dashboard/_components/createForm.jsx
@@ -2,16 +2,20 @@
 import { useState } from "react"
 import { Button } from "@/components/ui/button"
 import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
 } from "@/components/ui/dialog"
 
+import moment from 'moment/moment';
+import { db } from "@/configs/db";
+
 import ChatSession from '../../../configs/geminiModal'
+import { JsonForms } from "@/configs/schema";
 
 import { useUser } from '@clerk/nextjs';
 
@@ -19,40 +23,52 @@ import { useRouter } from 'next/router';
 const PROMPT = ", On the basis of description please give form in json format with form title, form subheading, Form field, form name, placeholder name, and form label,fieldType, field required In Json format"
 
 export default function CreateForm() {
-    const [userInput,setUserInput] = useState();
-    const[loading,setloading] = useState();
-    const {user} = useUser();
-    
-    const onGenerateForm = async()=>{
+    const [userInput, setUserInput] = useState();
+    const [loading, setloading] = useState();
+    const { user } = useUser();
+
+    const onGenerateForm = async () => {
         setloading(true)
 
-          const chatSession = await ChatSession; // Assuming ChatSession is a Promise
-    const result = await chatSession.sendMessage("Description:" + userInput + PROMPT);
+        const chatSession = await ChatSession; 
+        const result = await chatSession.sendMessage("Description:" + userInput + PROMPT);
         console.log(result.response.text())
-    }
-  return (
-    <Dialog>
-      <DialogTrigger asChild>
-        <Button variant="outline">Edit Profile</Button>
-      </DialogTrigger>
-      <DialogContent className="sm:max-w-[425px]">
-        <DialogHeader>
-          <DialogTitle>Enter  prompt </DialogTitle>
-          <DialogDescription>
-            Just a give a prompt create new form in an instant
-          </DialogDescription>
-        </DialogHeader>
-        <div className="grid gap-4 py-2">
-          <div className="grid grid-row-4 items-center gap-4">
+        if(result.response.text()){
+            const resp= await db.insert(JsonForms)
+            .values({
+                jsonform:result.response.text(),
+                createdBy:user?.primaryEmailAddress?.emailAddress,
+                createdAt: moment().format('DD/MM/YYYY')
+            }).returning({id:JsonForms.id})
+            console.log(resp[0].id)
            
-            <textarea className="textarea textarea-success" onChange={(e)=>setUserInput(e.target.value)} placeholder="Write the desrciption of your form"></textarea>
-          </div>
-    
-        </div>
-        <DialogFooter>
-          <Button type="submit" onClick={onGenerateForm} >Generate Form</Button>
-        </DialogFooter>
-      </DialogContent>
-    </Dialog>
-  )
+            setloading(false)
+        }
+            setloading(false)
+    }
+    return (
+        <Dialog>
+            <DialogTrigger asChild>
+                <Button variant="outline">Edit Profile</Button>
+            </DialogTrigger>
+            <DialogContent className="sm:max-w-[425px]">
+                <DialogHeader>
+                    <DialogTitle>Enter  prompt </DialogTitle>
+                    <DialogDescription>
+                        Just a give a prompt create new form in an instant
+                    </DialogDescription>
+                </DialogHeader>
+                <div className="grid gap-4 py-2">
+                    <div className="grid grid-row-4 items-center gap-4">
+
+                        <textarea className="textarea textarea-success" onChange={(e) => setUserInput(e.target.value)} placeholder="Write the desrciption of your form"></textarea>
+                    </div>
+
+                </div>
+                <DialogFooter>
+                    <Button type="submit" onClick={onGenerateForm} disabled={loading} >Generate Form</Button>
+                </DialogFooter>
+            </DialogContent>
+        </Dialog>
+    )
 }
---
+++ form-x/configs/db.ts
@@ -0,0 +1,13 @@
+import { drizzle } from "drizzle-orm/neon-http";
+import { neon } from "@neondatabase/serverless";
+import { config } from "dotenv";
+import * as schema from "./schema"
+config({ path: ".env.local" }); 
+
+
+if (!process.env.NEXT_PUBLIC_DATABASE_URL_CONFIG) {
+  throw new Error("NEXT_PUBLIC_DATABASE_URL_CONFIG is not defined");
+}
+const sql = neon(process.env.NEXT_PUBLIC_DATABASE_URL_CONFIG);
+
+export const db = drizzle(sql, { schema });
